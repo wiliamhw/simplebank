bf41a2f4a31e837227007fd9bb9fba43e4796eb2
William H.W <wiliamwijaya1985@gmail.com>
2022-04-23 00:49:29 +0700

    feat: implement database transaction and its test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -14,16 +14,20 @@ const (
 	dbSource = "postgresql://root:password@localhost:5432/simple_bank?sslmode=disable"
 )
 
-var testQueries *Queries
+var (
+	testQueries *Queries
+	testDB      *sql.DB
+)
 
 // Will be run before or after any other test functions.
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatalf("Cannot connect to db: %v", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	errCode := m.Run() // Run other test functions
 	os.Exit(errCode)
--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -1,6 +1,10 @@
 package db
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
 
 // Provides all functions to execute db queries and transactions.
 type Store struct {
@@ -13,4 +17,79 @@ func NewStore(db *sql.DB) *Store {
 		db:      db,
 		Queries: New(db),
 	}
+}
+
+// Executes a function within a database transaction.
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	if err := fn(q); err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rollback err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
+
+// Contains the input parameter of transfer transaction.
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// The result of the transfer transaction.
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+/**
+ * Performs a money transfer from one account to the other.
+ * It creates a transfer record, add account entries,
+ * and update account's balance within a single database transaction.
+ */
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (
+	TransferTxResult, error,
+) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams(arg))
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// TODO: update account's balance
+
+		return nil
+	})
+
+	return result, err
 }
